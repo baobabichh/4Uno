86028cf1c6c44c9541c86ea22e46d9ba1556e82c
baobabichh <pickup2legs@gmail.com>
2023-02-28 16:20:47 +0200

    added card functional

---
+++ src/Tests/CardTests.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "Uno/Card.h"
+#include "Lib/Test.h"
+
+TEST(CardTest_canBePlacedRegular)
+{
+	using namespace card;
+	constexpr Card cards_correct[]
+	{
+		{ CardImage::Zero, CardColor::Blue },
+		{ CardImage::Zero, CardColor::Green },
+		{ CardImage::One, CardColor::Green },
+		{ CardImage::One, CardColor::Red },
+		{ CardImage::Two, CardColor::Red },
+		{ CardImage::Two, CardColor::Yellow },
+		{ CardImage::PlusTwo, CardColor::Yellow },
+		{ CardImage::Skip, CardColor::Yellow },
+		{ CardImage::Skip, CardColor::Red },
+		{ CardImage::Reverse, CardColor::Red },
+		{ CardImage::Reverse, CardColor::Blue },
+		{ CardImage::ChangeColor, CardColor::Blue },
+		{ CardImage::ChangeColor, CardColor::Green },
+		{ CardImage::PlusFourChangeColor, CardColor::Blue },
+		{ CardImage::Zero, CardColor::Blue },
+	};
+
+	for (size_t i = 1; i < sizeof(cards_correct)/ sizeof(Card); i++)
+	{
+		if (!canBePlacedRegular(cards_correct[i - 1], cards_correct[i]))
+			return 0;
+	}
+
+	if (canBePlacedRegular({ CardImage::Zero, CardColor::Blue }, { CardImage::One, CardColor::Red }))
+		return 0;
+
+
+	return 1;
+}
+
+TEST(CardTest_canBePlacedSpecial)
+{
+	using namespace card;
+
+	// Plus two.
+	if (!canBePlacedSpecial({ CardImage::PlusTwo, CardColor::Blue }, { CardImage::PlusTwo, CardColor::Blue }))
+		return 0;
+
+	if (!canBePlacedSpecial({ CardImage::PlusTwo, CardColor::Blue }, { CardImage::PlusTwo, CardColor::Red }))
+		return 0;
+
+	if (canBePlacedSpecial({ CardImage::PlusTwo, CardColor::Blue }, { CardImage::PlusFourChangeColor, CardColor::Red }))
+		return 0;
+
+	if (canBePlacedSpecial({ CardImage::PlusTwo, CardColor::Blue }, { CardImage::ChangeColor, CardColor::Red }))
+		return 0;
+
+	// Skip.
+	if (!canBePlacedSpecial({ CardImage::Skip, CardColor::Blue }, { CardImage::Skip, CardColor::Blue }))
+		return 0;
+
+	if (canBePlacedSpecial({ CardImage::Skip, CardColor::Red }, { CardImage::Skip, CardColor::Blue }))
+		return 0;
+
+	// Plus four.
+	if (!canBePlacedSpecial({ CardImage::PlusFourChangeColor, CardColor::Blue }, { CardImage::PlusTwo, CardColor::Blue }))
+		return 0;
+
+	if (canBePlacedSpecial({ CardImage::PlusFourChangeColor, CardColor::Red }, { CardImage::PlusTwo, CardColor::Blue }))
+		return 0;
+
+
+	return 1;
+}
